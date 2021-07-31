e11c44496b40b80d8e65175305b1c064377a3d5d
sidnet1964 <sidnet@mail.ru>
2021-08-01 01:23:55 +0300

    finish addet

--- js/app.js
+++ js/app.js
@@ -38,9 +38,9 @@ function handleSubmit(evt){
     }
 
     const result = calculateCashback(specialAmount, otherAmount);
-    specialCashbackEl.textContent = `${result.specialCategoryCashback} руб.`;
-    otherCashbackEl.textContent = `${result.otherCategoryCashback} руб.`;
-    totalCashbackEl.textContent = `${result.totalCashback} руб.`;
+    specialCashbackEl.textContent = result.specialCategoryCashback.toFixed(0);
+    otherCashbackEl.textContent = result.otherCategoryCashback.toFixed(0);
+    totalCashbackEl.textContent = result.totalCashback.toFixed(0);
 }
 
 const formEl = document.getElementById('cashback-form');
