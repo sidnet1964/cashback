91c3eb45866d8a6d4a343d902a8bcf50cbf4ae17
sidnet1964 <sidnet@mail.ru>
2021-07-31 11:50:32 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const specialCategoryPurchases = 250000;
+const specialCategoryPercent = 0.03;
+const otherCategoryPurchases = 700000;
+const otherCategoryPercent = 0.01;
+const specialCategoryCashback = specialCategoryPurchases * specialCategoryPercent;
+const otherCategoryCashback = otherCategoryPurchases * otherCategoryPercent;
+let totalCashback = specialCategoryCashback + otherCategoryCashback;
+const limit = 15000;
+if (totalCashback > limit){
+    totalCashback = limit;
+}
+console.log(totalCashback);
